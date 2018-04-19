a7cbde2f3b07702d07874f75f4e4122c97eedcd0
cirelalespier <lespier.cirela@gmail.com>
2018-04-19 21:25:26 +0100

    ultima versao quinta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
 
-import { PrimeiroExemploComponent } from './primeiro-exemplo/primeiro-exemplo.component';
-import { SegundoExemploComponent } from './segundo-exemplo/segundo-exemplo.component';
-import { TerceiroExemploComponent } from './terceiro-exemplo/terceiro-exemplo.component';
+
+
 import { NoticiaModule } from './noticia/noticia.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    RodapeComponent,
-    
-    PrimeiroExemploComponent,
-    SegundoExemploComponent,
-    TerceiroExemploComponent
+    RodapeComponent    
   ],
   imports: [
     BrowserModule,
